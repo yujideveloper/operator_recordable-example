ec0485f496c4c20f4242f11999beb77af5127440
Yuji Hanamura <yuji.developer@gmail.com>
2018-09-13 01:20:33 +0900

    Remove debug codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action { OperatorRecordable.operator = Operator.find(Operator.ids.sample) }
-  before_action do
-    Rails.logger.debug OperatorRecordable.config.store.class
-    Rails.logger.debug Post.new.operator.id
-  end
 end
